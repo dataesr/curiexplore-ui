f0575135cf731d601e2708b74b9783a2d9941623
Jerem <jeremy.peglion@gmail.com>
2022-12-15 14:41:56 +0100

    add international cooperation ambassy description

--- src/components/HtmlAmbassyBloc/index.js
+++ src/components/HtmlAmbassyBloc/index.js
@@ -1,25 +1,23 @@
 import PropTypes from 'prop-types';
 import Parser from 'html-react-parser';
 
-import { Col, Container, Highlight, Row, Title } from '@dataesr/react-dsfr';
+import { Col, Container, Row, Title } from '@dataesr/react-dsfr';
 
 export default function HtmlAmbassyBloc({ data }) {
   if (!data) return null;
 
   return (
-    <Container>
+    <Container fluid>
       <Row>
         <Col>
-          <Title as="h2">
+          <Title as="h3">
             {data.thematique}
           </Title>
         </Col>
       </Row>
       <Row>
         <Col>
-          <Highlight>
-            {Parser(data.description)}
-          </Highlight>
+          {Parser(data.description)}
         </Col>
       </Row>
     </Container>
--- src/pages/country/components/international-cooperation.js
+++ src/pages/country/components/international-cooperation.js
@@ -1,7 +1,20 @@
+import { useOutletContext } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import HtmlAmbassyBloc from '../../../components/HtmlAmbassyBloc';
+
 export default function InternationalCooperationPage() {
+  const contextData = useOutletContext();
+  const data = contextData['curiexplore-analyse'];
+
+  const blocs = [];
+  blocs.push(data.find((el) => (el.fields.thematique === "Politique d'attractivité")).fields || null);
+  blocs.push(data.find((el) => (el.fields.thematique === 'Partenariats étrangers')).fields || null);
+
   return (
     <>
-      InternationalCooperationPage
+      {blocs.map((bloc) => (
+        <HtmlAmbassyBloc data={bloc} key={uuidv4()} />
+      ))}
     </>
   );
 }
