a8a58047dc767cb328952627dd6479c99e74bb8f
jerem <jeremy.peglion@gmail.com>
2023-03-21 11:38:09 +0100

    add IDH card in HigherEducation page

--- src/pages/country/components/higher-education/index.js
+++ src/pages/country/components/higher-education/index.js
@@ -1,7 +1,8 @@
 import { useOutletContext } from 'react-router-dom';
-import { Container, Col, Row } from '@dataesr/react-dsfr';
+import { Container, Col, Row, Title } from '@dataesr/react-dsfr';
 import ChartComponents from '../chart-components';
 import HtmlAmbassyBloc from '../../../../components/html-ambassy-bloc';
+import GenericCard from '../../../../components/generic-card';
 
 import charts from './charts.json';
 import PieChart from './components/donut/index';
@@ -10,8 +11,29 @@ import Pyramid from './components/pyramid/index';
 export default function CountryHigherEducationPage() {
   const contextData = useOutletContext();
   const data = contextData['curiexplore-analyse'];
+  const dataIDH = contextData['curiexplore-donnees-quantitatives'];
 
   const dataES = data.find((el) => (el.fields.thematique === 'Enseignement supérieur')).fields || null;
+
+  // Nombre moyen d'années de scolarité
+  const MOYSCO = { ...dataIDH.find((el) => el.fields.code === 'MOYSCO').fields };
+
+  // Espérance de scolarisation
+  const ESPSCO = { ...dataIDH.find((el) => el.fields.code === 'ESPSCO').fields };
+
+  const getDescription = (code) => {
+    if (code.code === 'MOYSCO') {
+      return (
+        <Title as="h3">{`${code.value.toFixed(1)} ${code.unit}`}</Title>
+      );
+    }
+    if (code.code === 'ESPSCO') {
+      return (
+        <Title as="h3">{`${code.value.toFixed(1)} ${code.unit}`}</Title>
+      );
+    }
+    return null;
+  };
 
   return (
     <Container>
@@ -28,6 +50,22 @@ export default function CountryHigherEducationPage() {
       <Row>
         <Col>
           <Pyramid />
+        </Col>
+      </Row>
+      <Row gutters>
+        <Col n="6">
+          <GenericCard
+            badgeLabel={MOYSCO.year}
+            description={getDescription(MOYSCO)}
+            title={MOYSCO.label}
+          />
+        </Col>
+        <Col n="6">
+          <GenericCard
+            badgeLabel={ESPSCO.year}
+            description={getDescription(ESPSCO)}
+            title={ESPSCO.label}
+          />
         </Col>
       </Row>
       <Row>
