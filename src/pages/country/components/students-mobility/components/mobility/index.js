803876c1e81e7fec059c3249c17491b786c7dc84
mialy <rakoto.mk@gmail.com>
2023-04-19 09:59:30 +0200

    (feat)mobility: mobility callout

---
+++ src/pages/country/components/students-mobility/components/mobility/index.js
@@ -0,0 +1,73 @@
+import { useParams } from 'react-router-dom';
+import { Col, Row, Text, Callout, CalloutText, CalloutTitle } from '@dataesr/react-dsfr';
+import { v4 as uuidv4 } from 'uuid';
+import useFetchData from '../../../../hooks/useFetchData';
+
+export default function MobilityCallOut() {
+  const { isoCode } = useParams();
+  const { data } = useFetchData(isoCode);
+
+  const dataMobilite = data['mobilite-internationale-etudiants'];
+  const latestYear = data['mobilite-internationale-etudiants']?.[0]?.fields.year;
+  let totalStudents = data['curiexplore-donnees-quantitatives'];
+
+  if (totalStudents && totalStudents?.length > 0) {
+    totalStudents = totalStudents.find((country) => (country.fields.code === '25053')
+    && (country.fields.year === latestYear))?.fields?.value;
+  }
+
+  let filteredData = [];
+  if (dataMobilite && dataMobilite?.length > 0) {
+    filteredData = dataMobilite.filter((country) => country.fields.year === latestYear);
+  }
+
+  let orderedData = [];
+  if (filteredData && filteredData?.length > 0) {
+    orderedData = filteredData.sort((a, b) => b.fields.value - a.fields.value).slice(0, 5);
+  }
+
+  const students = filteredData.map((el) => (
+    el.fields.value
+  ));
+
+  const sumStudents = students.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0,
+  );
+
+  const shareMobiliteStudents = (sumStudents * 100) / totalStudents;
+
+  return (
+    <Row>
+      <Col>
+        <Callout hasInfoIcon={false}>
+          <CalloutTitle>
+            {`Top 5 de la mobilité étudiante du pays en ${latestYear}`}
+          </CalloutTitle>
+          <CalloutText>
+            <Text>
+              En&nbsp;
+              {latestYear}
+              , on compte&nbsp;
+              {sumStudents.toLocaleString()}
+                &nbsp;étudiants internationaux soit&nbsp;
+              {shareMobiliteStudents.toFixed(2)}
+              &nbsp;% des étudiants en enseignement supérieur
+            </Text>
+            <ol>
+              {orderedData.map((el) => (
+                <li key={uuidv4()}>
+                  {`${el.fields.origin_country_fr} : ${el.fields.value.toLocaleString()} étudiants`}
+                </li>
+              ))}
+            </ol>
+          </CalloutText>
+        </Callout>
+      </Col>
+    </Row>
+  );
+}
+
+MobilityCallOut.propTypes = {
+
+};
--- src/pages/country/components/students-mobility/index.js
+++ src/pages/country/components/students-mobility/index.js
@@ -1,15 +1,12 @@
-import { useParams, useOutletContext } from 'react-router-dom';
-import { Container, Col, Row, Callout, CalloutText, CalloutTitle, Text } from '@dataesr/react-dsfr';
-import { v4 as uuidv4 } from 'uuid';
-import useFetchData from '../../hooks/useFetchData';
+import { useOutletContext } from 'react-router-dom';
+import { Container, Col, Row } from '@dataesr/react-dsfr';
 import ChartComponents from '../chart-components';
+import MobilityCallOut from './components/mobility';
 
 import HtmlAmbassyBloc from '../../../../components/html-ambassy-bloc';
 import charts from './charts.json';
 
 export default function StudentsMobilityPage() {
-  const { isoCode } = useParams();
-  const { data } = useFetchData(isoCode);
   const contextData = useOutletContext();
   const analyse = contextData['curiexplore-analyse'];
 
@@ -19,55 +16,9 @@ export default function StudentsMobilityPage() {
     blocs.push(analyse.find((el) => (el.fields.thematique === 'Mobilité sortante')).fields || null);
   }
 
-  const dataMobilite = data['mobilite-internationale-etudiants'];
-  const latestYear = data['mobilite-internationale-etudiants']?.[0]?.fields.year;
-
-  let filteredData = [];
-  if (dataMobilite && dataMobilite?.length > 0) {
-    filteredData = dataMobilite.filter((country) => country.fields.year === latestYear);
-  }
-
-  let orderedData = [];
-  if (filteredData && filteredData?.length > 0) {
-    orderedData = filteredData.sort((a, b) => b.fields.value - a.fields.value).slice(0, 5);
-  }
-
-  const students = filteredData.map((el) => (
-    el.fields.value
-  ));
-
-  const sumStudents = students.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0,
-  );
-
   return (
     <Container>
-      <Row>
-        <Col>
-          <Callout hasInfoIcon={false}>
-            <CalloutTitle>
-              {`Top 5 de la mobilité étudiante du pays en ${latestYear}`}
-            </CalloutTitle>
-            <CalloutText>
-              <Text>
-                En&nbsp;
-                {latestYear}
-                , on compte&nbsp;
-                {sumStudents.toLocaleString()}
-                &nbsp;étudiants internationaux soit [X]% des étudiants
-              </Text>
-              <ol>
-                {orderedData.map((el) => (
-                  <li key={uuidv4()}>
-                    {`${el.fields.origin_country_fr} : ${el.fields.value.toLocaleString()} étudiants`}
-                  </li>
-                ))}
-              </ol>
-            </CalloutText>
-          </Callout>
-        </Col>
-      </Row>
+      <MobilityCallOut />
       <Row>
         <Col>
           {blocs.map((bloc) => (
