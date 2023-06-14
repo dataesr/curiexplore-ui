3244624defdc5211a003c348080a2e3b56923c58
mialy <rakoto.mk@gmail.com>
2023-06-14 14:25:54 +0200

    fix homepage filter

--- src/pages/homepage/utils/regions.js
+++ src/pages/homepage/utils/regions.js
@@ -5,6 +5,6 @@ export default {
   'Amérique du nord': 'North America',
   'Amérique du sud': 'Latin America & Caribbean',
   'Asie-Océanie': 'East Asia & Pacific',
-  'Europe continentale': 'Europe & Central Asia',
+  'Europe continentale': 'Continental europe',
   'Union Européenne et Turquie': 'Europe & Central Asia',
 };
