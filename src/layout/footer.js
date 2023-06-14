78090f32d2d762dcca1319b019146b5efb56ee29
mialy <rakoto.mk@gmail.com>
2023-06-14 12:36:00 +0200

    adding footer bottom

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,40 +1,60 @@
+import { Link as RouterLink } from 'react-router-dom';
 import {
   Footer as FooterWrapper,
   FooterBody,
   FooterBodyItem,
-  Link,
+  FooterBottom,
+  FooterLink,
   Logo,
 } from '@dataesr/react-dsfr';
 
 export default function Footer() {
   return (
     <FooterWrapper>
-      <FooterBody description="CurieXplore : La plateforme d'exploration dessystèmes d'enseignement supérieur, de recherche et d'innovationà l'international">
+      <FooterBody description="CurieXplore : La plateforme d'exploration des systèmes d'enseignement supérieur, de recherche et d'innovation à l'international">
         <Logo
-          asLink={<Link href="https://www.enseignementsup-recherche.gouv.fr/fr" />}
+          asLink={<FooterLink href="https://www.enseignementsup-recherche.gouv.fr/fr" />}
           splitCharacter={9}
         >
           Gouvernement
         </Logo>
         <FooterBodyItem>
-          <Link target="_blank" href="https://legifrance.gouv.fr">
+          <FooterLink target="_blank" href="https://legifrance.gouv.fr">
             legifrance.gouv.fr
-          </Link>
+          </FooterLink>
         </FooterBodyItem>
         <FooterBodyItem>
-          <Link target="_blank" href="https://gouvernement.fr">
+          <FooterLink target="_blank" href="https://gouvernement.fr">
             gouvernement.fr
-          </Link>
+          </FooterLink>
         </FooterBodyItem>
         <FooterBodyItem>
-          <Link target="_blank" href="https://service-public.fr">
+          <FooterLink target="_blank" href="https://service-public.fr">
             service-public.fr
-          </Link>
+          </FooterLink>
         </FooterBodyItem>
         <FooterBodyItem>
-          <Link target="_blank" href="https://data.gouv.fr">data.gouv.fr</Link>
+          <FooterLink target="_blank" href="https://data.gouv.fr">data.gouv.fr</FooterLink>
         </FooterBodyItem>
       </FooterBody>
+      <FooterBottom>
+        <FooterLink asLink={<RouterLink to="/aide" />}>
+          Aide
+        </FooterLink>
+        <FooterLink asLink={<RouterLink to="/projet-et-equipe" />}>
+          L'équipe et son projet
+        </FooterLink>
+        <FooterLink asLink={<RouterLink to="/nous-contacter" />}>
+          Nous contacter
+        </FooterLink>
+        <FooterLink asLink={<FooterLink href="https://github.com/orgs/dataesr/repositories?q=curiexplore-ui-v2&type=&language=&sort=" target="_blank" />}>
+          Github
+        </FooterLink>
+        <FooterLink target="_blank" href={`https://github.com/dataesr/curiexplore-ui-v2/releases/tag/v${process.env.REACT_APP_VERSION}`}>
+          {`Version de l'application v${process.env.REACT_APP_VERSION}`}
+        </FooterLink>
+      </FooterBottom>
+
     </FooterWrapper>
   );
 }
