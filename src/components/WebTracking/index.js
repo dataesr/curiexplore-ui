57ccd9b6c5223b9db846f60b21b8975ddebc7b7e
jerem <jeremy.peglion@gmail.com>
2023-08-07 10:38:53 +0200

    add matomo tracker

---
+++ src/components/WebTracking/index.js
@@ -0,0 +1,21 @@
+import { createInstance, MatomoProvider } from '@datapunt/matomo-tracker-react';
+import PropTypes from 'prop-types';
+
+function WebTracking({ children }) {
+  const instance = createInstance({
+    urlBase: 'https://piwik.enseignementsup-recherche.pro',
+    siteId: 41,
+    disabled: false,
+    configurations: {
+      disableCookies: true,
+    },
+  });
+
+  return <MatomoProvider value={instance}>{children}</MatomoProvider>;
+}
+
+WebTracking.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default WebTracking;
