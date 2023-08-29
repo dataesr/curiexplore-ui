5b95aa6eddce5e7d59250f2c7d45142b4794fe39
mialy <rakoto.mk@gmail.com>
2023-08-29 17:58:43 +0200

    fix(api): change apis

--- server/src/routes/opendatasoft.js
+++ server/src/routes/opendatasoft.js
@@ -6,9 +6,38 @@ router.route('/opendatasoft')
   .get(async (req, res) => {
     try {
       const query = req?.query;
+      let paramsCode = '';
+      const codes = query['refine.code'];
+      if (typeof codes === 'object') {
+        delete query['refine.code'];
+
+        let i = 0;
+        while (i < codes.length) {
+          paramsCode += `&refine.code=${codes[i]}`;
+          i += 1;
+        }
+      } else {
+        paramsCode = '';
+      }
+
+      let paramsCountry = '';
+      const countries = query['refine.country_code'];
+      if (typeof countries === 'object') {
+        delete query['refine.country_code'];
+
+        let i = 0;
+        while (i < codes.length) {
+          paramsCountry += `&refine.code=${countries[i]}`;
+          i += 1;
+        }
+      } else {
+        paramsCountry = '';
+      }
+
       if (query) {
         const response = await fetch(
-          `${process.env.REACT_APP_ODS_API_URL}/?apikey=${process.env.REACT_APP_ODS_API_KEY}&${new URLSearchParams(query).toString()}`,
+          // eslint-disable-next-line max-len
+          `${process.env.REACT_APP_ODS_API_URL}/?apikey=${process.env.REACT_APP_ODS_API_KEY}&${new URLSearchParams(query).toString()}${paramsCountry}${paramsCode}`,
         );
         const jsonData = await response.json();
         res.status(200).json(jsonData);
--- server/src/routes/paysage.js
+++ server/src/routes/paysage.js
@@ -5,19 +5,15 @@ const router = new express.Router();
 router.route('/paysage/*')
   .get(async (req, res) => {
     try {
-      const url = req?.params?.[0];
-      if (url) {
-        const response = await fetch(
-          `${process.env.REACT_APP_PAYSAGE_API_URL}/${url}`,
-          {
-            headers: { 'X-API-KEY': process.env.REACT_APP_PAYSAGE_API_KEY },
-          },
-        );
-        const jsonData = await response.json();
-        res.status(200).json(jsonData);
-      } else {
-        res.status(200).json({});
-      }
+      const url = `${process.env.REACT_APP_PAYSAGE_API_URL}${req.url.replace('/paysage', '')}`;
+      const response = await fetch(
+        url,
+        {
+          headers: { 'X-API-KEY': process.env.REACT_APP_PAYSAGE_API_KEY },
+        },
+      );
+      const jsonData = await response.json();
+      res.status(200).json(jsonData);
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: 'Internal Server Error.' });
