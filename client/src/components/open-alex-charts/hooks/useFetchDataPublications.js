c319893541de5cabf1fbe4813e605f8aa0f01c58
mialy <rakoto.mk@gmail.com>
2023-08-30 17:53:17 +0200

    feat(api): openalex config

--- client/src/components/open-alex-charts/hooks/useFetchDataPublications.js
+++ client/src/components/open-alex-charts/hooks/useFetchDataPublications.js
@@ -6,16 +6,14 @@ import { useEffect, useState } from 'react';
 exportingModule(Highcharts);
 exportingData(Highcharts);
 
-const { REACT_APP_OPENALEX_RANGE, REACT_APP_OPENALEX_URL } = process.env;
-
 export default function useFetchData(isoCode) {
   const [data, setData] = useState([]);
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const query = `${REACT_APP_OPENALEX_URL}&filter=publication_year:${REACT_APP_OPENALEX_RANGE},institutions.country_code:${isoCode},institutions.country_code:fr&group_by=publication_year`;
-
+    // eslint-disable-next-line max-len
+    const query = `${process.env.REACT_APP_CURIEXPLORE_API}/openalex?&filter=publication_year:${process.env.REACT_APP_OPENALEX_RANGE},institutions.country_code:${isoCode},institutions.country_code:fr&group_by=publication_year`;
     const getData = async () => {
       try {
         setIsLoading(true);
