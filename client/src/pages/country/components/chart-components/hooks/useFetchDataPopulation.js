80354e859052d479f2011dab2cb3905dc421dfa6
Mihoub Debache <debache.mihoub@gmail.com>
2023-08-30 14:53:48 +0200

    fix(useFetchDataPopulation): avoid error

--- client/src/pages/country/components/chart-components/hooks/useFetchDataPopulation.js
+++ client/src/pages/country/components/chart-components/hooks/useFetchDataPopulation.js
@@ -13,21 +13,28 @@ export default function useFetchData(data) {
       setIsLoading(true);
       const baseUrl = `${process.env.REACT_APP_CURIEXPLORE_API}/opendatasoft?dataset=curiexplore-donnees-quantitatives&rows=-1&disjunctive.code=true&disjunctive.country_code=true`;
       const url = `${baseUrl}&refine.code=${data.code}&sort=${data.sort}&refine.country_code=${data.countryCode}`;
-      const response = await fetch(url);
-      const json = await response.json();
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
 
-      setLastYear(json.records[json.records.length - 1].fields.year);
-      setLastYearData(formatNumber(json.records[json.records.length - 1].fields.value, 2).toLocaleString());
-      const value = Math.round((json.records[json.records.length - 1].fields.value * 100) / json.records[0].fields.value - 100);
-      let valueToPrint = '';
-      valueToPrint = `${(value > 0) ? '+' : ''}${value.toString()} %`;
-      setEvolution({
-        label: `${json.records[0].fields.year} -> ${json.records[json.records.length - 1].fields.year}`,
-        data: valueToPrint,
-      });
-      setIsLoading(false);
+        if (json.records.length === 0) { return null; }
+        setLastYear(json?.records[json.records.length - 1].fields.year);
+        setLastYearData(formatNumber(json.records[json.records.length - 1].fields.value, 2).toLocaleString());
+        const value = Math.round((json.records[json.records.length - 1].fields.value * 100) / json.records[0].fields.value - 100);
+        const valueToPrint = `${(value > 0) ? '+' : ''}${value.toString()} %`;
+        setEvolution({
+          label: `${json.records[0].fields.year} -> ${json.records[json.records.length - 1].fields.year}`,
+          data: valueToPrint,
+        });
+        return {};
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        return {};
+      } finally {
+        setIsLoading(false);
+      }
     };
-
     getData();
   }, [data.code, data.countryCode, data.sort]);
 
